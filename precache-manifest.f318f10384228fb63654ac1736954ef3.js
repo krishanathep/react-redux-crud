917c72e8416d2b0af0edb9f1c1a6f62dff994ec1
krishanathep <krishanathep@gmail.com>
2020-05-25 09:47:41 +0700

    Updates

---
+++ precache-manifest.f318f10384228fb63654ac1736954ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8add2253f35a5c024c615301c045ef",
+    "url": "/react-redux-crud/index.html"
+  },
+  {
+    "revision": "c452e80705504da3b2cd",
+    "url": "/react-redux-crud/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91eb586f2d31bbed68e2",
+    "url": "/react-redux-crud/static/js/2.c7baa089.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-crud/static/js/2.c7baa089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c452e80705504da3b2cd",
+    "url": "/react-redux-crud/static/js/main.385c0da7.chunk.js"
+  },
+  {
+    "revision": "786aab2477bbbb31a3b0",
+    "url": "/react-redux-crud/static/js/runtime-main.6b56b170.js"
+  }
+]);
